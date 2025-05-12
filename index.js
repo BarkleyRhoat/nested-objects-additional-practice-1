57fdfb93b09c9103028c0a8904f088f9299a7f5d
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-05-12 09:54:09 -0400

    Add notes to test

--- index.js
+++ index.js
@@ -3,6 +3,50 @@ const { streamingLibrary } = require("./streamingLibrary.js")
 // Example usage of the streamingLibrary
 console.log("streamingLibrary: ", streamingLibrary)
 
+const genreNames = () => {
+  // Your code here
+}
+
+const getShowTitlesByGenre = () => {
+  // Your code here
+}
+
+const getEpisodeTitlesByShow = () => {
+  // Your code here
+}
+
+const highRatedEpisodes = () => {
+  // Your code here
+}
+
+const totalEpisodes = () => {
+  // Your code here
+}
+
+const showsWithHighRatedEpisodes = () => {
+  // Your code here
+}
+
+const getEpisodesByShowAndSeason = () => {
+  // Your code here
+}
+
+const addEpisode = () => {
+  // Your code here
+}
+
+const getHighestRatedEpisode = () => {
+  // Your code here
+}
+
+const getEpisodesSortedByRating = () => {
+  // Your code here
+}
+
+const getAverageRatingForShow = () => {
+  // Your code here
+}
+
 // ===================================================================================================
 // The rest of this is required to allow the test script to find the functions you will define here.
 // ===================================================================================================
--- test/indexTest.js
+++ test/indexTest.js
@@ -15,61 +15,77 @@ const {
 } = require("../index.js")
 
 describe("Streaming Library solutions", () => {
-  it("Challenge 1: should list all genre names", () => {
+  it("genreNames should list all genre names", () => {
     const names = genreNames()
     expect(names).to.be.an("array")
     expect(names).to.not.be.empty
     expect(names).to.include.members(["Science Fiction", "Comedy"])
   })
 
-  it("Challenge 2: should return correct shows for a given genre", () => {
+  it("getShowTitlesByGenre should return correct shows for a given genre", () => {
     const titles = getShowTitlesByGenre("Science Fiction")
+    // expect to equal to the exact matches
     expect(titles).to.include("Galaxy Quest")
   })
 
-  it("Challenge 3: should return episode titles for a given show", () => {
+  it("getEpisodeTitlesByShow should return episode titles for a given show", () => {
     const episodes = getEpisodeTitlesByShow("Galaxy Quest")
+    // this should test for the exact matches
+    // ie include ["ep 1", "ep 2", "ep 3"]
     expect(episodes).to.include("Into the Void")
   })
 
-  it("Challenge 4: should find episodes with rating > 9", () => {
-    expect(highRatedEpisodes.some((ep) => ep.rating > 9)).to.be.true
+  it("highRatedEpisodes should find episodes with rating > 9", () => {
+    episodes = highRatedEpisodes()
+    // this expects that at least one episode has a rating > 9
+    // which would allow for lower ratings
+    // Needs to be updated
+    expect(episodes.some((ep) => ep.rating > 9)).to.be.true
   })
 
-  it("Challenge 5: should count total number of episodes", () => {
-    expect(totalEpisodes).to.be.a("number")
-    expect(totalEpisodes).to.be.above(0)
+  it("totalEpisodes should count total number of episodes", () => {
+    total = totalEpisodes()
+    expect(total).to.be.a("number")
+    expect(total).to.be.above(0)
+    // this needs to be more specific
   })
 
-  it("Challenge 6: should find shows with episodes rated >= 9", () => {
-    expect(showsWithHighRatedEpisodes.length).to.be.greaterThan(0)
+  it("showsWithHighRatedEpisodes should find shows with episodes rated >= 9", () => {
+    const shows = showsWithHighRatedEpisodes()
+    expect(shows.length).to.be.greaterThan(0)
+    // add tests here
   })
 
-  it("Challenge 7: should list episodes from specific season of a show", () => {
+  it("getEpisodesByShowAndSeason should list episodes from specific season of a show", () => {
     const episodes = getEpisodesByShowAndSeason("Galaxy Quest", 1)
+    // change this to an equal instead of include
     expect(episodes).to.include("The Wormhole")
   })
 
-  it("Bonus 8: should add a new episode", () => {
+  it("addEpisode should add a new episode", () => {
     const newEp = { episodeNumber: 99, title: "Bonus Episode", rating: 9.5 }
     addEpisode("Galaxy Quest", 1, newEp)
     const episodes = getEpisodesByShowAndSeason("Galaxy Quest", 1)
+    // change this to an equal instead of include
     expect(episodes).to.include("Bonus Episode")
   })
 
-  it("Bonus 9: should find the highest-rated episode", () => {
+  it("getHighestRatedEpisode should find the highest-rated episode", () => {
     const highest = getHighestRatedEpisode()
     expect(highest.rating).to.be.greaterThan(0)
+    // this needs to be more specific
   })
 
-  it("Bonus 10: should sort episodes of a show by rating", () => {
+  it("getEpisodesSortedByRating should sort episodes of a show by rating", () => {
     const sorted = getEpisodesSortedByRating("Galaxy Quest")
     expect(sorted[0].rating).to.be.at.least(sorted[1].rating)
+    // this needs to be more specific
   })
 
-  it("Bonus 11: should get average rating for a show", () => {
+  it("getAverageRatingForShow should get average rating for a show", () => {
     const avg = getAverageRatingForShow("Galaxy Quest")
     expect(parseFloat(avg)).to.be.a("number")
     expect(parseFloat(avg)).to.be.above(0)
+    // this needs to be more specific
   })
 })
