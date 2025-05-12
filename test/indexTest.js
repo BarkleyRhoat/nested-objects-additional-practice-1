054b60d81a055049e6ac40cde97abceb20b51174
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-05-12 09:17:46 -0400

    Refactor tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,64 +1,75 @@
-const { expect } = require('chai');
-const path = require('path');
+require("./helpers.js")
 
-const solutions = require(path.resolve(__dirname, '../index'));
+const {
+  genreNames,
+  getShowTitlesByGenre,
+  getEpisodeTitlesByShow,
+  highRatedEpisodes,
+  totalEpisodes,
+  showsWithHighRatedEpisodes,
+  getEpisodesByShowAndSeason,
+  addEpisode,
+  getHighestRatedEpisode,
+  getEpisodesSortedByRating,
+  getAverageRatingForShow,
+} = require("../index.js")
 
-describe('Streaming Library solutions', () => {
-  it('Challenge 1: should list all genre names', () => {
-    expect(solutions.genreNames).to.be.an('array');
-    expect(solutions.genreNames).to.not.be.empty;
-    expect(solutions.genreNames).to.include.members(['Science Fiction', 'Comedy']);
-  });
+describe("Streaming Library solutions", () => {
+  it("Challenge 1: should list all genre names", () => {
+    const names = genreNames()
+    expect(names).to.be.an("array")
+    expect(names).to.not.be.empty
+    expect(names).to.include.members(["Science Fiction", "Comedy"])
+  })
 
-  it('Challenge 2: should return correct shows for a given genre', () => {
-    const titles = solutions.getShowTitlesByGenre('Science Fiction');
-    expect(titles).to.include('Galaxy Quest');
-  });
+  it("Challenge 2: should return correct shows for a given genre", () => {
+    const titles = getShowTitlesByGenre("Science Fiction")
+    expect(titles).to.include("Galaxy Quest")
+  })
 
-  it('Challenge 3: should return episode titles for a given show', () => {
-    const episodes = solutions.getEpisodeTitlesByShow('Galaxy Quest');
-    expect(episodes).to.include('Into the Void');
-  });
+  it("Challenge 3: should return episode titles for a given show", () => {
+    const episodes = getEpisodeTitlesByShow("Galaxy Quest")
+    expect(episodes).to.include("Into the Void")
+  })
 
-  it('Challenge 4: should find episodes with rating > 9', () => {
-    expect(solutions.highRatedEpisodes.some(ep => ep.rating > 9)).to.be.true;
-  });
+  it("Challenge 4: should find episodes with rating > 9", () => {
+    expect(highRatedEpisodes.some((ep) => ep.rating > 9)).to.be.true
+  })
 
-  it('Challenge 5: should count total number of episodes', () => {
-    expect(solutions.totalEpisodes).to.be.a('number');
-    expect(solutions.totalEpisodes).to.be.above(0);
-  });
+  it("Challenge 5: should count total number of episodes", () => {
+    expect(totalEpisodes).to.be.a("number")
+    expect(totalEpisodes).to.be.above(0)
+  })
 
-  it('Challenge 6: should find shows with episodes rated >= 9', () => {
-    expect(solutions.showsWithHighRatedEpisodes.length).to.be.greaterThan(0);
-  });
+  it("Challenge 6: should find shows with episodes rated >= 9", () => {
+    expect(showsWithHighRatedEpisodes.length).to.be.greaterThan(0)
+  })
 
-  it('Challenge 7: should list episodes from specific season of a show', () => {
-    const episodes = solutions.getEpisodesByShowAndSeason('Galaxy Quest', 1);
-    expect(episodes).to.include('The Wormhole');
-  });
+  it("Challenge 7: should list episodes from specific season of a show", () => {
+    const episodes = getEpisodesByShowAndSeason("Galaxy Quest", 1)
+    expect(episodes).to.include("The Wormhole")
+  })
 
-  // Bonus solutions
-  it('Bonus 8: should add a new episode', () => {
-    const newEp = { episodeNumber: 99, title: 'Bonus Episode', rating: 9.5 };
-    solutions.addEpisode('Galaxy Quest', 1, newEp);
-    const episodes = solutions.getEpisodesByShowAndSeason('Galaxy Quest', 1);
-    expect(episodes).to.include('Bonus Episode');
-  });
+  it("Bonus 8: should add a new episode", () => {
+    const newEp = { episodeNumber: 99, title: "Bonus Episode", rating: 9.5 }
+    addEpisode("Galaxy Quest", 1, newEp)
+    const episodes = getEpisodesByShowAndSeason("Galaxy Quest", 1)
+    expect(episodes).to.include("Bonus Episode")
+  })
 
-  it('Bonus 9: should find the highest-rated episode', () => {
-    const highest = solutions.getHighestRatedEpisode();
-    expect(highest.rating).to.be.greaterThan(0);
-  });
+  it("Bonus 9: should find the highest-rated episode", () => {
+    const highest = getHighestRatedEpisode()
+    expect(highest.rating).to.be.greaterThan(0)
+  })
 
-  it('Bonus 10: should sort episodes of a show by rating', () => {
-    const sorted = solutions.getEpisodesSortedByRating('Galaxy Quest');
-    expect(sorted[0].rating).to.be.at.least(sorted[1].rating);
-  });
+  it("Bonus 10: should sort episodes of a show by rating", () => {
+    const sorted = getEpisodesSortedByRating("Galaxy Quest")
+    expect(sorted[0].rating).to.be.at.least(sorted[1].rating)
+  })
 
-  it('Bonus 11: should get average rating for a show', () => {
-    const avg = solutions.getAverageRatingForShow('Galaxy Quest');
-    expect(parseFloat(avg)).to.be.a('number');
-    expect(parseFloat(avg)).to.be.above(0);
-  });
-});
+  it("Bonus 11: should get average rating for a show", () => {
+    const avg = getAverageRatingForShow("Galaxy Quest")
+    expect(parseFloat(avg)).to.be.a("number")
+    expect(parseFloat(avg)).to.be.above(0)
+  })
+})
