e3f4c256be1ead18fc98e3988590a328fea01660
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-08 09:38:25 -0400

    completed first two tests

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-const { streamingLibrary } = require("./streamingLibrary.js")
+const { streamingLibrary } = require("./streamingLibrary.js");
 
-// Example usage of the streamingLibrary
-console.log("streamingLibrary: ", streamingLibrary)
+console.log("streamingLibrary: ", streamingLibrary);
 
 const genreNames = () => {
-  // Your code here
-}
+  return streamingLibrary.genres.map((g) => g.name);
+};
 
-const getShowTitlesByGenre = () => {
-  // Your code here
-}
+const getShowTitlesByGenre = (genreName) => {
+  const genre = streamingLibrary.genres.find((g) => g.name === genreName);
+  return genre.shows.map((show) => show.title);
+};
 
 const getEpisodeTitlesByShow = () => {
   // Your code here
-}
+};
 
 const highRatedEpisodes = () => {
   // Your code here
-}
+};
 
 const totalEpisodes = () => {
   // Your code here
-}
+};
 
 const showsWithHighRatedEpisodes = () => {
   // Your code here
-}
+};
 
 const getEpisodesByShowAndSeason = () => {
   // Your code here
-}
+};
 
 const addEpisode = () => {
   // Your code here
-}
+};
 
 const getHighestRatedEpisode = () => {
   // Your code here
-}
+};
 
 const getEpisodesSortedByRating = () => {
   // Your code here
-}
+};
 
 const getAverageRatingForShow = () => {
   // Your code here
-}
+};
 
 // ===================================================================================================
 // The rest of this is required to allow the test script to find the functions you will define here.
@@ -62,4 +62,4 @@ module.exports = {
   getHighestRatedEpisode,
   getEpisodesSortedByRating,
   getAverageRatingForShow,
-}
+};
